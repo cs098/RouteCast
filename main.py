542f4189c9f8daf04667a5108325faf7ce80b8ab
Declasaurus <119758972+Declasaurus@users.noreply.github.com>
2024-02-02 11:11:57 -0500

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ asklocation = input(
     "What is your location? Ex. -123,-23- or -Henrico- or -Henrico Virginia- ")
 askhours = input(
     "For what hour today would you like to get the forecast for? ")
+
 # Make a GET request to the URL
 response = requests.get(
     'https://api.weatherapi.com/v1/forecast.json?key=d64b86a0b5be44fd9d3160958242901&q='
@@ -12,6 +13,7 @@ response = requests.get(
 # Get the JSON data from the response
 data = response.json()
 # Display the forecast info
+# The [0] in the data is for the day. 0 is current day, 1 is tomorrow, 2 is day after tomorrow.
 print("This forecast is for the county of " + data['location']['name'] +
       " in the state/area of " + data['location']['region'])
 print("The temperature at", askhours, "hours will be",
@@ -32,3 +34,6 @@ print("The uv index will be",
 print(
     "the chance of raining will be", data['forecast']['forecastday'][0]['hour']
     [int(askhours)]['chance_of_rain'], "%")
+print(
+"the temp tomorrow is", data['forecast']['forecastday'][1]['hour']
+[int(askhours)]['temp_f'], "%")
