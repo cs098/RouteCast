5d982beb7106176cfe9367ca624f80f24ba6e38a
cs098 <124285392+cs098@users.noreply.github.com>
2024-02-04 18:48:06 -0500

    Update script.js with displayRoute
    Added code to display route between pointA and pointB.
    I tried to create a separate method for this, but it was giving me errors, so if anyone knows how to put it into a separate method feel free.

--- script.js
+++ script.js
@@ -8,6 +8,26 @@ function initMap() {
 
   });
 
+  //route between two points (change to accomodate more than two points later)
+  var pointA = new google.maps.LatLng(37.682819, -77.587799); //lat and lng will be user inputted
+  var pointB = new google.maps.LatLng(37.682819, -76.587799);
+  const directionsService = new google.maps.DirectionsService;
+  const directionsDisplay = new google.maps.DirectionsRenderer({map: map});
+  
+  //calculates and displays route
+  directionsService.route({
+    origin: pointA,
+    destination: pointB,
+    travelMode: google.maps.TravelMode.DRIVING //set to driving mode for now (can be changed later if needed)
+  },
+    function(response, status) {
+      if (status == google.maps.DirectionsStatus.OK) {
+        directionsDisplay.setDirections(response);
+      } else {
+        alert("failed to display");
+      }
+  });
+
 }
 
 window.initMap = initMap;
