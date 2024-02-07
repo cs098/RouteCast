867f072f520a4c294389d4ae3ef1963e636aef0a
cs098 <124285392+cs098@users.noreply.github.com>
2024-02-07 09:53:47 -0500

    Update script.js with multiple points on route

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 function initMap() {
+
+  //for displaying route
+  const directionsService = new google.maps.DirectionsService();
+  const directionsRenderer = new google.maps.DirectionsRenderer();
+
+  //initializes map
   const map = new google.maps.Map(document.getElementById("map"), {
     zoom: 10,
     center: { lat: 37.682819, lng: -77.587799 },
@@ -7,29 +13,43 @@ function initMap() {
     mapTypeControl: false
 
   });
-
-  //route between two points (change to accomodate more than two points later)
-  var pointA = new google.maps.LatLng(37.682819, -77.587799); //lat and lng will be user inputted
-  var pointB = new google.maps.LatLng(37.682819, -76.587799);
-  const directionsService = new google.maps.DirectionsService;
-  const directionsDisplay = new google.maps.DirectionsRenderer({map: map});
   
-  //calculates and displays route
-  directionsService.route({
-    origin: pointA,
-    destination: pointB,
-    travelMode: google.maps.TravelMode.DRIVING //set to driving mode for now (can be changed later if needed)
-  },
-    function(response, status) {
-      if (status == google.maps.DirectionsStatus.OK) {
-        directionsDisplay.setDirections(response);
-      } else {
-        alert("failed to display");
-      }
+  //something to do with rendering route?
+  directionsRenderer.setMap(map);
+  
+  //calls function below
+  calculateAndDisplayRoute(directionsService, directionsRenderer);
+}
+
+function calculateAndDisplayRoute(directionsService, directionsRenderer) {
+  
+  //destinations on route
+  var start = "Richmond, VA";
+  var end = "New York, New York";
+  var waypts = []; //holds object for each stop
+  //add a loop later to push every stop we need (see if latitudes and longitudes work?)***
+  waypts.push({
+    location: "Maryland, DE",
+    stopover: true
   });
 
+  //calculates route
+  directionsService
+    .route({
+      origin: start,
+      destination: end,
+      waypoints: waypts,
+      optimizeWaypoints: true,
+      travelMode: google.maps.TravelMode.DRIVING, //mode can be changed if needed
+    },
+      //checks if locations are valid 
+      function(response, status) {
+        if (status == google.maps.DirectionsStatus.OK) {
+          directionsRenderer.setDirections(response); //renders route
+        } else {
+          alert("failed to display route"); //fail message
+        }
+      });
 }
 
 window.initMap = initMap;
-
-
